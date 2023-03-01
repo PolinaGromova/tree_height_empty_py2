dd0638b7774ac9036a314879c43e89667bbafd1e
Poļina Gromova <124886886+PolinaGromova@users.noreply.github.com>
2023-03-01 19:26:26 +0000

    Polina testing

--- tree_height.py
+++ tree_height.py
@@ -2,26 +2,52 @@
 
 import sys
 import threading
-import numpy
 
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    root = None
+    position = [[] for _ in range(n)]
+    for i in range(n):
+        if parents[i] == -1:
+            root=i 
+        else:
+            position[parents[i]].append(i)
+
+    def max_height(b):
+        heigth=1
+        if not position[b]:
+            return heigth
+        else:
+            for child in position[b]:    
+                heigth=max(heigth,max_height(child))
+            return heigth+1    
+    return max_height(root)
+    
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    text=input("Enter I or F: ")
+    if "I" in text:
+        n=int(input())
+        parents=list(map(int,input().split()))
+    elif "F" in text:
+        filename=input()    
+        file='./test/'+filename
+        if "a" not in filename:
+            try:
+                with open(file) as f:
+                    n=int(f.readline())
+                    parents=list(map(int,f.readline().split()))
+            except Exception as e:
+                print("Error:(", str(e))
+                return
+        else:
+            print("Error: invalid filename")   
+            return  
+    else:
+        print("Enter I or F: ")  
+        return   
+    print(compute_height(n, parents))                 
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
